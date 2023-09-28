b2857c2f7c51afd6bcb7888b51b2d451a1c773de
khaleelsheikh <khaleels.workmail@gmail.com>
2023-09-28 10:49:34 +0300

    Created Signup page

--- app/signup/SignUpPage.js
+++ app/signup/SignUpPage.js
@@ -1,21 +1,21 @@
 // Import the necessary Next.js components
 import Link from "next/link";
 
-export default function LoginPage() {
+export default function SignUp() {
   return (
     <>
-      <div className="flex justify-center mt-12">
+      <div className="flex justify-center">
         <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
           <form className="space-y-6" action="#">
             <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-              Sign in to our platform
+              Sign up for a new account
             </h5>
             <div>
               <label
                 htmlFor="email"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Your email
+                Your Email
               </label>
               <input
                 type="email"
@@ -23,6 +23,38 @@ export default function LoginPage() {
                 id="email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 placeholder="name@company.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Enter your first name"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="last_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="last_name"
+                id="last_name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Enter your last name"
                 required
               />
             </div>
@@ -42,38 +74,35 @@ export default function LoginPage() {
                 required
               />
             </div>
-            <div className="flex items-start">
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label
-                  htmlFor="remember"
-                  className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Remember me
-                </label>
-              </div>
+            <div>
+              <label
+                htmlFor="repeat_password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="repeat_password"
+                id="repeat_password"
+                placeholder="Confirm your password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+              />
             </div>
             <button
               type="submit"
               className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Login to your account
+              Create Account
             </button>
             <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-              Not registered?{" "}
+              Already registered?{" "}
               <Link
-                href="/signup"
+                href="/login"
                 className="text-blue-700 hover:underline dark:text-blue-500"
               >
-                Create account
+                Login
               </Link>
             </div>
           </form>
---
+++ app/signup/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React, { useState } from "react";
+import Header from "../components/Header";
+import SignUp from "./SignUpPage";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <Header />
+      <SignUp />
+    </>
+  );
+}
