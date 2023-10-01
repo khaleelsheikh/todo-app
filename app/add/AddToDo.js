5d4ec9941fb6f1b3f0967cfc9b6bce2537a50232
khaleelsheikh <khaleels.workmail@gmail.com>
2023-10-01 17:19:08 +0300

    writing apis

--- app/add/AddToDo.js
+++ app/add/AddToDo.js
@@ -12,15 +12,15 @@ export default function AddTodo({ addToDo }) {
 
   return (
     <>
-      <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form class="space-y-6" action="#">
-          <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form className="space-y-6" action="#">
+          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
             Add your To Do's
           </h5>
           <div>
             <label
-              for="title"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="title"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Title
             </label>
@@ -28,14 +28,14 @@ export default function AddTodo({ addToDo }) {
               type="text"
               name="title"
               id="title"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               required
             />
           </div>
           <div>
             <label
-              for="title"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="desc"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Description
             </label>
@@ -43,13 +43,13 @@ export default function AddTodo({ addToDo }) {
               type="text"
               name="desc"
               id="desc"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               required
             />
           </div>
           <button
-            onSubmit={handleSubmit}
-            class="w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={handleSubmit}
+            className="w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add To Do
           </button>
---
+++ app/api/[userId]/todos/route.js
@@ -0,0 +1,7 @@
+export function GET(request) {}
+
+export function POST(request) {}
+
+export function PUT(request) {}
+
+export function DELETE(request) {}
