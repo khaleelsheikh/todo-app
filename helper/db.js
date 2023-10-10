8e635fc2dde15cacd9996799ee92209825110d37
khaleelsheikh <khaleels.workmail@gmail.com>
2023-10-10 17:07:58 +0300

    coding to connect to db

--- helper/db.js
+++ helper/db.js
@@ -2,5 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-  } catch (error) {}
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "To-Do App",
+    });
+    console.log("Mongo DB Connected...");
+    console.log(connection);
+  } catch (error) {
+    console.log("Failed to connect with database");
+    console.log(error);
+  }
 };
