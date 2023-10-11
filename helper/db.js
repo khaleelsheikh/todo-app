cb36566e601987f5ebe3e137a281249badd5f478
khaleelsheikh <khaleels.workmail@gmail.com>
2023-10-11 16:51:13 +0300

    debugging connection to db

--- helper/db.js
+++ helper/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+    const { connection } = await mongoose.connect(process.env.MONGO_DB_URL, {
       dbName: "To-Do App",
     });
     console.log("Mongo DB Connected...");
